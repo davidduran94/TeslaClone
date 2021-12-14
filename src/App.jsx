aa8a48480e8581b33e1b44612f4d92bd999573b3
David Duran <davidduran94@gmail.com>
2021-12-13 20:41:06 -0600

    add configs and header

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import './App.css';
 import Header from './components/Header'
 import Home from './components/Home'
 
-function App() {
+const App = function () {
   return (
+    <>
+      <Header />
       <Home />
+    </>
   );
-}
+};
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
 
-function Header() {
+const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <Container>
+            <a>
+                <img src='/images/logo.svg'/>
+            </a>
+            <Menu>
+
+            </Menu>
+        </Container>
     )
 }
 
 export default Header
+
+const Container = styled.div`
+    min-height: 60px;
+    position: fixed;
+    display: flex;
+    align-items: center;
+    padding: 0 20px;
+
+`
+
+const Menu = styled.div`
+ 
+`
